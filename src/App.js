704e51558b6ca0bf3b76ceb4693d60a0e51d9009
Pawel Domagala <domagalapawel11@gmail.com>
2023-03-19 21:54:01 +0100

    Add Timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import Container from './components/Container/Container';
+import Button from './components/Button/Button';
+import Timer from './components/Timer/Timer';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+	const [time, setTime] = useState(0);
+	const [timer, setTimer] = useState('');
+	const timerStart = () => {
+		setTimer(setInterval(() => {
+			setTime(prevValue => prevValue + 10);
+		}, 1));
+	};
+	useEffect(() => {
+		return () => {
+			if(timer) clearInterval(timer);
+		};
+	}, [timer]);
+	const timerStop = () => {
+		clearInterval(timer);
+	};
+	const timerReset = () => {
+		setTime(0);
+	};
 	return (
-		<div className="App">
-		</div>
+		<Container>
+			<Timer time={time}/>
+			<div className='row'>
+				<Button action={timerStart}>Start</Button>
+				<Button action={timerStop}>Stop</Button>
+				<Button action={timerReset}>Reset</Button>
+			</div>
+		</Container>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 const container = document.querySelector('#root');
 const root = ReactDOMClient.createRoot(container);
